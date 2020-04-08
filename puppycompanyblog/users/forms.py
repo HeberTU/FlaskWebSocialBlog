04e11cbed4888ac7a6fd802e4e7241a67db602a8
Heber <heber.trj.urt@gmail.com>
2020-04-08 18:00:51 +0200

    Update forma has been Added

--- puppycompanyblog/users/forms.py
+++ puppycompanyblog/users/forms.py
@@ -19,6 +19,21 @@ class Register(FlaskForm):
     username = StringField('UserName: ', validators=[DataRequired()])
     password = PasswordField('Password: ', validators=[DataRequired(), EqualTo('pass_confirm', message = 'Passwords must match.' )])
     pass_confirm = PasswordField('Confirm Password: ', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(f'{field.data} has been registered')
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(f'{field.data} has been registered')
+
+
+class UpdateForm(FlaskForm):
+    email = StringField('Email: ', validators = [DataRequired(),Email()])
+    username = StringField('UserName: ', validators = [DataRequired()])
+    picture = FileField('Update Profile Picturee: ', validators = [FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
