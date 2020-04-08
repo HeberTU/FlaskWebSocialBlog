0e4e06f0864e0788a8b3da3a6d5eda1fc1d64d7a
Heber <heber.trj.urt@gmail.com>
2020-04-08 14:43:10 +0200

    Data Base Set-Up using SQLAlchemy within init file

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,7 +1,22 @@
 # Puppy company blog/__init__.py
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+
+######################
+## Data Base Set-UP ##
+######################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']  = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
 
 from puppycompanyblog.core.views import core
 from puppycompanyblog.error_pages.handlers import error_pages
