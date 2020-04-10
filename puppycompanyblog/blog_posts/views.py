df40abb7c76ab1fd251dfba01fd23fed7fe77a9d
Heber <heber.trj.urt@gmail.com>
2020-04-10 16:04:17 +0200

    Update and Delete views were added

--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -15,11 +15,11 @@ def create_post():
 
     if form.validate_on_submit():
 
-        blog_posts = BlogPost(title=form.title.data,
+        blog_post = BlogPost(title=form.title.data,
                               text = form.text.data,
                               user_id = current_user.id)
 
-        db.session.add(blog_posts)
+        db.session.add(blog_post)
         db.session.commit()
         flash('Blog Post Created')
         return redirect(url_for('core.index'))
@@ -27,7 +27,7 @@ def create_post():
     return render_template('create_post.html', form = form)
 
 # Read (View) a Blog Post
-@blog_posts.route('/<int:blog_post_id>', methods = ['GET','POST'])
+@blog_posts.route('/<int:blog_post_id>')
 def blog_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
     return render_template('blog_post.html', title = blog_post.title,
@@ -35,4 +35,36 @@ def blog_post(blog_post_id):
 
 
 # Update a Blog Post
+@blog_posts.route('/<int:blog_post_id>/update', methods = ['GET','POST'])
+@login_required
+def update(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        abort(403)
+    form = BlogPostForm()
+    if form.validate_on_submit():
+
+        blog_post.title=form.title.data
+        blog_post.text = form.text.data
+        db.session.commit()
+        flash('Blog Post Updated')
+        return redirect(url_for('blog_posts.blog_post', blog_post_id=blog_posts.id))
+
+    elif request.method = 'GET':
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+
+    return render_template('create_post.html', title="updating", form= form)
+
+
 # Delete a Blog Post
+@blog_posts.route('/<int:blog_post_id>/delete', methods = ['GET','POST'])
+@login_required
+def delete_post(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        abort(403)
+    db.session.delete(blog_post)
+    db.session.commit()
+    flash('Blog Post Deleted')
+    return redirect(url_for('core.idex'))
